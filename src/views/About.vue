aea1674e025d8abadc3b248c98b5b0bd08de3c25
danizaghian <dani.zaghian@gmail.com>
2018-10-25 13:36:58 -0700

    adds vuejs-todo-app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div id="map"></div>
   </div>
 </template>
+
+<style>
+  /* Always set the map height explicitly to define the size of the div
+   * element that contains the map. */
+  #map {
+    height: 500px;
+  }
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  methods: {},
+  mounted: function() {
+  	var map;
+	  map = new google.maps.Map(document.getElementById('map'), {
+	    center: { lat: 37.773972, lng: -122.431297 },
+	    zoom: 11
+	  });
+  },
+  computed: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,110 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+		<h1>Todo List: </h1>
+
+		<div>
+	    <button v-on:click="removeCompletedTasks()">Clear Completed Tasks</button>
+	  </div>
+
+	  <button v-on:click="toggleTaskFilter(false)">View Incomplete</button>
+	  <button v-on:click="toggleTaskFilter(true)">View Complete</button>
+	  <button v-on:click="toggleTaskFilter('all')">View All</button>
+
+	  <div>
+	  	Search Tasks: <input v-model="taskFilter" list="taskText">
+	  </div>
+
+	  <datalist id="taskText">
+	  	<option v-for="task in tasks">{{ task.text }}</option>
+	  </datalist>
+
+    <div v-for="task in filterBy(tasks, taskFilter)" v-on:click="toggleCompleted(task)">
+      <h4 :class="{strike: task.completed}"> {{ task.text }} </h4>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+	.strike {
+		text-decoration: line-through;
+	}
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      tasks: [
+      	{
+      		id: 1,
+      		text: "Mow the lawn",
+      		completed: false
+      	},
+      	{
+      		id: 2,
+      		text: "Brush the cat",
+      		completed: true
+      	},
+      	{
+      		id: 3,
+      		text: "Eat your veggies",
+      		completed: false
+      	},
+      	{
+      		id: 4,
+      		text: "Do the dishes",
+      		completed: false
+      	},
+      	{
+      		id: 5,
+      		text: "Learn to code",
+      		completed: false
+      	}
+      ],
+      taskFilter: ''
+    };
+  },
+  methods: {
+    toggleCompleted: function(task) {
+      task.completed = !task.completed;
+    },
+    removeCompletedTasks: function() {
+	    var incompleteTasks = [];
+	    this.tasks.forEach(function(task){
+	    	if (!task.completed) {
+	        incompleteTasks.push(task);
+	      }
+	    });
+	    this.tasks = incompleteTasks;
+    },
+    toggleTaskFilter: function(filter) {
+    	if(filter === true) {
+    		this.taskFilter = true;
+    	} else if(filter === 'all'){
+    		this.taskFilter = '';
+    	} else {
+    		this.taskFilter = false;
+    	}
+    }
+  },
+  computed: {}
+};
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
