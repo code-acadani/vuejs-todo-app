62434e3b229107343ac33bfc8ec89d5df66fdad6
danizaghian <dani.zaghian@gmail.com>
2018-10-29 13:24:16 -0700

    adds places array and markers

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,48 +17,53 @@
 /* global google */
 export default {
   data: function() {
-    return {};
+    return {
+      places: [
+        { 
+          lat: 37.773972, 
+          lng: -122.431297, 
+          description: "This is lower haight" 
+        },
+        { 
+          lat: -25.363, 
+          lng: 131.044, 
+          description: "This is somewhere in Australia!" 
+        },
+        { 
+          lat: -33.8675, 
+          lng: 151.207, 
+          description: "This is Sydney!" 
+        },
+        { 
+          lat: 20, 
+          lng: -105, 
+          description: "This is Cacaluta!" 
+        }
+      ]
+    };
   },
   methods: {},
   mounted: function() {
     var sanFran = { lat: 37.773972, lng: -122.431297, description: "This is a marker!" };
 	  var map = new google.maps.Map(document.getElementById('map'), {
 	    center: sanFran,
-	    zoom: 12
+	    zoom: 4
 	  });
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">San Francisco</h1>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-        'sandstone rock formation in the southern part of the '+
-        'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-        'south west of the nearest large town, Alice Springs; 450&#160;km '+
-        '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-        'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-        'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-        'Aboriginal people of the area. It has many springs, waterholes, '+
-        'rock caves and ancient paintings. Uluru is listed as a World '+
-        'Heritage Site.</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
 
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
+    this.places.forEach(function(place){
+      var infowindow = new google.maps.InfoWindow({
+        content: place.description
+      });
+
+      var marker = new google.maps.Marker({
+        position: place,
+        map: map
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
     });
 
-    var marker = new google.maps.Marker({
-      position: sanFran,
-      map: map,
-      title: 'San Francisco (Lower Haight)'
-    });
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
   },
   computed: {}
 };
